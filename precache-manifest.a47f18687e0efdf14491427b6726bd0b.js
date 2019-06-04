59d12ae389b94a0a38f283f028e61b6669bbf3cb
Petermorgangh <peter@1080peter.com>
2019-06-04 11:11:23 -0600

    Updates

---
+++ precache-manifest.a47f18687e0efdf14491427b6726bd0b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac901016ee914fdb4d2fba4b5d63a6a",
+    "url": "/jotter-react/index.html"
+  },
+  {
+    "revision": "70bf4080c1ff26da8d08",
+    "url": "/jotter-react/static/js/2.84411e73.chunk.js"
+  },
+  {
+    "revision": "ea14c242994363a4ebbc",
+    "url": "/jotter-react/static/js/main.28adfea0.chunk.js"
+  },
+  {
+    "revision": "cd26f4324b03d62fff91",
+    "url": "/jotter-react/static/js/runtime~main.89ec18a0.js"
+  }
+]);
